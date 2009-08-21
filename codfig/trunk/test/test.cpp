884a136324454c4d603fa24e647c1316f09582db
trainmeditation <trainmeditation@ad170a46-ef28-0410-90b8-bf340ce6c238>
2009-08-21 15:51:04 +0000

    Add fixture to test

--- codfig/trunk/test/test.cpp
+++ codfig/trunk/test/test.cpp
@@ -12,26 +12,28 @@ using std::endl;
 using codfig::Config;
 using codfig::ApplicationID;
 
-Config fixture() {
-	Config testConfig(ApplicationID("Test App", "0.0.0", "Shaun Bouckaert"));
+Config * fixture() {
+	Config * testConfig = new Config(ApplicationID("Test App", "0.0.0", "Shaun Bouckaert"));
 
-	testConfig.addSection("accounts");
-	testConfig["accounts"].addSection("isp");
-	testConfig["accounts.isp"].addSection("smtp");
-	testConfig("accounts.isp.smtp.ip").setValue(string("127.0.0.1"));
-	testConfig("accounts.isp.smtp.port").setValue(80);
+	(*testConfig).addSection("accounts");
+	(*testConfig)["accounts"].addSection("isp");
+	(*testConfig)["accounts.isp"].addSection("smtp");
+	(*testConfig)("accounts.isp.smtp.ip").setValue<string>("127.0.0.1");
+	(*testConfig)("accounts.isp.smtp.port").setValue(80);
 
-	testConfig.selectProfile(testConfig.addProfile("alternate isp"));
-	testConfig.addSection("accounts");
-	testConfig["accounts"].addSection("isp");
-	testConfig["accounts.isp"].addSection("smtp");
-	testConfig("accounts.isp.smtp.ip").setValue(string("127.0.0.2"));
-	testConfig("accounts.isp.smtp.port").setValue(81);
+	(*testConfig).selectProfile((*testConfig).addProfile("alternate isp"));
+	(*testConfig).addSection("accounts");
+	(*testConfig)["accounts"].addSection("isp");
+	(*testConfig)["accounts.isp"].addSection("smtp");
+	(*testConfig)("accounts.isp.smtp.ip").setValue<string>("127.0.0.2");
+	(*testConfig)("accounts.isp.smtp.port").setValue(81);
 
 	return testConfig;
 }
 
 int main(int argc, char * argv []) {
-	Config testConfig = fixture();
+	Config * testConfig = fixture();
+        cout << (*testConfig)("accounts.isp.smtp.ip").getValue<string>() << endl;
+        delete testConfig;
 	return 0;
 }
