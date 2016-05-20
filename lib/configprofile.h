a0d96096c9837c3f9cad9ff4c0cb22bdd4c25155
Shaun Bouckaert <shaun@train-meditations.com>
2016-05-20 16:03:14 +1000

    Fixed copyright headers

--- lib/configprofile.h
+++ lib/configprofile.h
@@ -1,3 +1,23 @@
+/*
+ * Copyright © 2009-2011, 2015-2016 Shaun Bouckaert
+ *
+ *  This file is part of Codfig.
+ *
+ *  Codfig is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Codfig is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Codfig.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 #ifndef TRAINMEDITATIONS_CONFIGPROFILE_H
 #define TRAINMEDITATIONS_CONFIGPROFILE_H
 
