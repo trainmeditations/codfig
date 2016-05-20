d009bcbc5ca74eae837dd723f2675c2e7f6c5c24
Shaun Bouckaert <shaun@train-meditations.com>
2016-05-20 15:12:53 +1000

    Moved ConfigValue to its own translation unit

---
+++ lib/configvalue.cpp
@@ -0,0 +1,58 @@
+/*
+ * Copyright © 2009-2011, 2015-2016 Shaun Bouckaert
+ *
+ *  This file is part of Codfig.
+ *
+ *  Codfig is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Codfig is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Codfig.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "configvalue.h"
+
+using namespace codfig;
+
+ConfigValue::AbstractValueBox::~AbstractValueBox()
+{}
+
+ConfigValue::ConfigValue():_value(NULL)
+{}
+
+ConfigValue::ConfigValue(const ConfigValue & other):_value(NULL)
+{
+    if (other._value) _value = other._value->cloneValue();
+}
+
+ConfigValue & ConfigValue::operator=(const ConfigValue & other)
+{
+    if (this != &other) {
+        delete _value;
+        if (other._value) {
+            _value = other._value->cloneValue();
+        } else {
+            _value = NULL;
+        }
+    }
+    return *this;
+}
+
+string ConfigValue::stringValue() const
+{
+    return _value->getStringValue();
+}
+
+ConfigValue::~ConfigValue()
+{
+    if (_value) delete _value;
+}
+
---
+++ lib/configvalue.h
@@ -0,0 +1,74 @@
+/*
+ * Copyright © 2009-2011, 2015-2016 Shaun Bouckaert
+ *
+ *  This file is part of Codfig.
+ *
+ *  Codfig is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Codfig is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Codfig.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef TRAINMEDITATIONS_CONFIGVALUE_H
+#define TRAINMEDITATIONS_CONFIGVALUE_H
+
+#include <string>
+
+using std::string;
+
+namespace codfig {
+    class ConfigValue {
+    protected:
+        class AbstractValueBox {
+        public:
+            virtual AbstractValueBox * cloneValue() const = 0;
+            virtual ~AbstractValueBox() = 0;
+            virtual string getStringValue() const = 0;
+        };
+
+        template <class T> class ValueBox:public AbstractValueBox {
+        public:
+            ValueBox();
+            ValueBox(T value);
+            ~ValueBox();
+            ValueBox<T> * cloneValue() const;
+            inline const T &value() const {return _value;}
+            inline T &value() { return _value;}
+            string getStringValue() const;
+            void setValueByString(const string &stringValue);
+        private:
+            T _value;
+        };
+
+    public:
+        ConfigValue();
+        ConfigValue(const ConfigValue & other);
+        ConfigValue & operator=(const ConfigValue & rhs);
+        virtual ~ConfigValue();
+        string stringValue() const;
+        template <class T>
+            const T &value() const;
+        template <class T>
+            T &value();
+        int intValue();
+    private:
+        AbstractValueBox * _value;
+    };
+}
+
+/************************
+* Template Definitions *
+************************/
+
+#include "configvalue_t.h"
+
+#endif // TRAINMEDITATIONS_CONFIGVALUE_H
